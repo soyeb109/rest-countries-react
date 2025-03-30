d374a73b6334cbee1f3604acaf794c42be793ef7
Md. Soyeb Islam <soyeb10191@gmail.com>
2025-03-30 14:05:28 +0600

    lift up state

--- src/components/Countries.jsx
+++ src/components/Countries.jsx
@@ -1,17 +1,33 @@
-import React, { use } from "react";
+import React, { use, useState } from "react";
 import Country from "./Country";
 import "./Countries.css";
 
 export default function Countries({ countriesPromise }) {
+  const [visitedCountries, setVisitedCountries] = useState([]);
+
   const countries = use(countriesPromise);
-  console.log(countries);
+
+  const handleVisitedCountries = (country) => {
+    const newVisitedCountries = [...visitedCountries, country];
+    setVisitedCountries(newVisitedCountries);
+  };
 
   return (
     <>
       <h1>Travelling Countries: {countries.length}</h1>
+      <h3>Traveled So Far: {visitedCountries.length}</h3>
+      <ol>
+        {visitedCountries.map((country) => (
+          <li key={country.cca3}>{country.name.common}</li>
+        ))}
+      </ol>
       <div className="countries">
         {countries.map((country) => (
-          <Country country={country} />
+          <Country
+            key={country.cca3}
+            country={country}
+            handleVisitedCountries={handleVisitedCountries}
+          />
         ))}
       </div>
     </>
--- src/components/Country.jsx
+++ src/components/Country.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import "./Country.css";
 
-const Country = ({ country }) => {
+const Country = ({ country, handleVisitedCountries }) => {
   const [visited, setVisited] = useState(false);
 
   const handleVisited = () => {
@@ -11,12 +11,17 @@ const Country = ({ country }) => {
     //   setVisited(true);
     // }
     setVisited(!visited);
+    handleVisitedCountries();
   };
 
   return (
     <div className={`country ${visited && "country-visited"}`}>
       <h3>Name: {country.name.common} </h3>
       <img src={country.flags.png} alt={country.flags.alt} />
+      <h4>
+        Independent:{" "}
+        {country.independent === true ? "Independent" : "Not Independent"}
+      </h4>
       <h2>Population: {country.population}</h2>
       <button onClick={handleVisited}>
         {visited ? "Visited" : "Not Visited"}
