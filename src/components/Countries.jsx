18c7b6047e7ca30ad0e4b84ea4a39ab75c44d1d6
Md. Soyeb Islam <soyeb10191@gmail.com>
2025-03-30 13:26:39 +0600

    add styles

--- src/components/Countries.jsx
+++ src/components/Countries.jsx
@@ -1,16 +1,19 @@
 import React, { use } from "react";
 import Country from "./Country";
+import "./Countries.css";
 
 export default function Countries({ countriesPromise }) {
   const countries = use(countriesPromise);
   console.log(countries);
 
   return (
-    <div>
+    <>
       <h1>Travelling Countries: {countries.length}</h1>
-      {countries.map((country) => (
-        <Country country={country} />
-      ))}
-    </div>
+      <div className="countries">
+        {countries.map((country) => (
+          <Country country={country} />
+        ))}
+      </div>
+    </>
   );
 }
--- src/components/Country.jsx
+++ src/components/Country.jsx
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import "./Country.css";
 
 const Country = ({ country }) => {
+  const [visited, setVisited] = useState(false);
+
+  const handleVisited = () => {
+    // if (visited === true) {
+    //   setVisited(false);
+    // } else {
+    //   setVisited(true);
+    // }
+    setVisited(!visited);
+  };
+
   return (
-    <div>
+    <div className={`country ${visited && "country-visited"}`}>
       <h3>Name: {country.name.common} </h3>
       <img src={country.flags.png} alt={country.flags.alt} />
+      <h2>Population: {country.population}</h2>
+      <button onClick={handleVisited}>
+        {visited ? "Visited" : "Not Visited"}
+      </button>
     </div>
   );
 };
